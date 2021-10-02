bad0dad3bcfbbc51350fed7f6c6db1a819c39cea
fredrik burman <fredrik.burman@gmail.com>
2021-10-02 15:48:58 +0200

    feat: now we can handle reinstalls of the app, ie if the merchant already had the app installed before but we didnt handle the uninstall properly

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -2,9 +2,6 @@ package api
 
 import (
 	_ "github.com/ColumbiaRoad/cr-shopify-upsell/backend/api/docs"
-
-	//_ "goStarter/demo/docs"
-
 	_ "github.com/labstack/echo/v4/middleware"
 
 	echoSwagger "github.com/swaggo/echo-swagger"
@@ -15,7 +12,6 @@ import (
 func (s *Server) Routes() {
 	s.Router.GET("/health", s.handleHealth())
 	v1 := s.Router.Group("/v1")
-
 	// Swagger docs
 	v1.GET("/docs/*", echoSwagger.WrapHandler)
 	v1.GET("/shopify/", s.handleInstall())
--- backend/app/merchant/merchant.go
+++ backend/app/merchant/merchant.go
@@ -4,8 +4,9 @@ import "context"
 
 // Storage is the interface to interact with the database, the methods are implemented in db/
 type Storage interface {
-	PersistToken(ctx context.Context, shop_url, access_token string) (int64, error)
-	CheckMerchantByShop(ctx context.Context, shopURL string) (bool, error)
+	PersistToken(ctx context.Context, shop_url, accessToken string) (int64, error)
+	UpdateToken(ctx context.Context, merchantID int64, accessToken string) (int64, error)
+	CheckMerchantByShopURL(ctx context.Context, shopURL string) (int64, error)
 }
 
 type merchant struct {
@@ -21,10 +22,17 @@ func New(storage Storage) Merchants {
 
 // Merchants represents the interface to "Users"
 type Merchants interface {
-	HandleInstall(ctx context.Context, shop_url, access_token string) (int64, error)
+	HandleInstall(ctx context.Context, shop_url, accessToken string) (int64, error)
 }
 
-func (m *merchant) HandleInstall(ctx context.Context, shop_url, access_token string) (int64, error) {
-	merchantID, err := m.storage.PersistToken(ctx, shop_url, access_token)
+func (m *merchant) HandleInstall(ctx context.Context, shop_url, accessToken string) (int64, error) {
+	existsID, err := m.storage.CheckMerchantByShopURL(ctx, shop_url)
+	if err != nil {
+		return 0, err
+	}
+	if existsID > 0 {
+		return m.storage.UpdateToken(ctx, existsID, accessToken)
+	}
+	merchantID, err := m.storage.PersistToken(ctx, shop_url, accessToken)
 	return merchantID, err
 }
