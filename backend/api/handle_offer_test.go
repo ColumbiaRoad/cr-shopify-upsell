3d71b7ae8ef881b9b2cd43e084abc8189c794a57
fredrik burman <fredrik.burman@gmail.com>
2021-10-03 21:27:55 +0200

    feat: added a way to replace dependencies during testing

--- backend/api/handle_offer_test.go
+++ backend/api/handle_offer_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 
 	"net/http"
@@ -11,13 +12,34 @@ import (
 	"github.com/matryer/is"
 )
 
-func TestOfferEndpoint(t *testing.T) {
+func TestOffer(t *testing.T) {
 	is := is.NewRelaxed(t)
 	fx := setTestFixture()
-	r := servertest.Get(fx.api, "/v1/offer")
+	fx.merchant.getVariantIDForShop = func(ctx context.Context, shopURL string) (int64, error) {
+		return 123123, nil
+	}
+	r := servertest.Get(fx.api, "/v1/offer?shop=offset-demo.myshopify.com")
 	is.Equal(r.Code, http.StatusOK)
 	var offerResponse Offer
 	err := json.NewDecoder(r.Body).Decode(&offerResponse)
 	is.NoErr(err)
 	is.Equal(offerResponse.ProductTitle, "Climate Compensation")
+	is.Equal(offerResponse.VariantId, int64(123123))
+}
+
+func TestOfferWithoutShopQueryParam(t *testing.T) {
+	is := is.NewRelaxed(t)
+	fx := setTestFixture()
+	r := servertest.Get(fx.api, "/v1/offer")
+	is.Equal(r.Code, http.StatusBadRequest)
+}
+
+func TestOfferForUnknownShop(t *testing.T) {
+	is := is.NewRelaxed(t)
+	fx := setTestFixture()
+	fx.merchant.getVariantIDForShop = func(ctx context.Context, shopURL string) (int64, error) {
+		return 0, nil
+	}
+	r := servertest.Get(fx.api, "/v1/offer?shop=offset-demo.myshopify.com")
+	is.Equal(r.Code, http.StatusNotFound)
 }
