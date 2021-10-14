cb2eaa01e9db8d255146fad2d7512d6fad853489
Pablo Núñez <pablo.nunez@columbiaroad.com>
2021-10-14 15:24:35 +0200

    feat: render based on echo value

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+	import Onboarding from "./components/Onboarding.svelte";
+	import Dashboard from "./components/Dashboard.svelte";
+
+	export let subscribed;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if subscribed}
+		<Dashboard />
+	{:else}
+		<Onboarding />
+	{/if}
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
--- backend/api/handle_shopify_install.go
+++ backend/api/handle_shopify_install.go
@@ -43,6 +43,7 @@ func (s *Server) handleShopify() echo.HandlerFunc {
 		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
 			"shop":   shopName,
 			"apiKey": s.Shopify.ApiKey,
+			"subscribed": true, // TODO: get from db
 		})
 
 	}
