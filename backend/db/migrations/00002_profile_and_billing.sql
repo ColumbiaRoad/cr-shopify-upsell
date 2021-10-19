a2ccef846a9f9262fc2652ab46ba300a306c8d14
Pablo Núñez <pablo.nunez@columbiaroad.com>
2021-10-19 15:28:00 +0200

    wip: attempt to add column

--- backend/db/migrations/00002_profile_and_billing.sql
+++ backend/db/migrations/00002_profile_and_billing.sql
@@ -5,8 +5,11 @@ ALTER TABLE "merchants" ADD COLUMN IF NOT EXISTS
     "subscription_id"   BIGINT DEFAULT 0;
 ALTER TABLE "merchants" ADD COLUMN IF NOT EXISTS
     "accepted_terms_at" TIMESTAMP WITH TIME ZONE;
+ALTER TABLE "merchants" ADD COLUMN IF NOT EXISTS
+    "should_render" BOOLEAN DEFAULT FALSE;
 
 
 -- +goose Down
 ALTER TABLE "merchants" DROP COLUMN "subscription_id";
 ALTER TABLE "merchants" DROP COLUMN "accepted_terms_at";
+ALTER TABLE "merchants" DROP COLUMN "should_render";
